4c4f9040a22a5beffdb34cb26ff0f2977cc1df61
yiuriel <sebastian.murawczik@gmail.com>
2025-02-12 22:26:36 +0100

    fix ui of form pages

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -8,7 +8,7 @@ type ButtonProps = PropsWithChildren<{
 export const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
   return (
     <button
-      className={`text-white bg-indigo-600 p-2
+      className={`text-white bg-indigo-600 py-2 px-4
       hover:bg-indigo-700 hover:text-white disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500
       rounded-lg
       ${className}
--- frontend/src/pages/Forms.tsx
+++ frontend/src/pages/Forms.tsx
@@ -3,6 +3,9 @@ import { queryKeys } from "../lib/queryKeys";
 import { api } from "../services/api";
 import { Loading } from "../components/Loading";
 import { FormCard } from "../components/FormCard/FormCard";
+import { Button } from "../components/Button/Button";
+import { useNavigate } from "react-router";
+import { useCallback } from "react";
 
 export const Forms = () => {
   const { data, isLoading } = useQuery({
@@ -10,14 +13,23 @@ export const Forms = () => {
     queryFn: api.feedbackForm.getAll,
   });
 
+  const navigate = useNavigate();
+
+  const goToForm = useCallback(() => {
+    navigate("/app/create-feedback-form");
+  }, [navigate]);
+
   if (isLoading) {
     return <Loading />;
   }
 
   return (
-    <div className="container mx-auto p-4 gap-4 flex flex-col">
-      <h1 className="text-3xl font-bold">Forms</h1>
-      <div className="grid grid-cols-2 gap-4">
+    <div className="container mx-auto py-10 gap-4 flex flex-col">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold">Forms</h1>
+        <Button onClick={goToForm}>Create new form</Button>
+      </div>
+      <div className="grid grid-cols-2 mt-8 gap-4">
         {data?.map((form) => (
           <FormCard key={form.id} form={form} />
         ))}
