909ce0243424e24b0906e6827207d8a878ee20a4
yiuriel <sebastian.murawczik@gmail.com>
2025-02-24 22:23:24 +0100

    add helper

---
+++ frontend/src/lib/downloadCsv.ts
@@ -0,0 +1,9 @@
+export const downloadCsv = (blob: Blob, filename: string) => {
+  const url = window.URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", filename);
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+};
